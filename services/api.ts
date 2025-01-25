abb9c2a9c526ab7e663e86d06244ad4fa6851d21
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2025-01-25 12:32:17 +0600

    Specialist profile

--- services/api.ts
+++ services/api.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 
 // Create the Axios instance
 const api = axios.create({
-  baseURL: "http://192.168.0.106:8080",
+  baseURL: "http://192.168.0.127:8080",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const mlAPI = axios.create({
-  baseURL: "http://192.168.0.106:8000",
+  baseURL: "http://192.168.0.127:8000",
   headers: {
     "Content-Type": "application/json",
   },
