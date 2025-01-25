45a4a9acb392d016fa1f83ab9a56064dd203a24d
tasniaiffat <tasniaiffat@gmail.com>
2025-01-26 01:24:34 +0600

    styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { AntDesign, FontAwesome } from '@expo/vector-icons'
-import { Image, StyleSheet } from 'react-native'
-import MyHeader from '@/components/MyHeader'
+import { Image, StyleSheet, View } from 'react-native'
 import MyText from '@/components/MyText'
-import { grey, lightblue } from "../../constants/Colors";
+import { grey } from "../../constants/Colors";
 
 export default function TabRoot() {
   return (
@@ -14,20 +13,14 @@ export default function TabRoot() {
         headerStyle: {
           backgroundColor: "#dfe4e0", 
         },
-        headerLeft: () => (
-          <Image 
-            source={require('@/assets/images/LocatoLogo-transparent.png')}
-            style={styles.logo}
-          />
-        ),
         headerTitle: () => (
-          <MyText textLabel='Locato' textColor={grey}></MyText>
-        ),
-        headerRight: () => (
-          <Image 
-            source={require('@/assets/images/profile.png')} 
-            style={styles.userImage}
-          />
+          <View style={styles.headerCenter}>
+            <Image 
+              source={require('@/assets/images/LocatoLogo-transparent.png')}
+              style={styles.logo}
+            />
+            <MyText textLabel='Locato' textColor={grey}></MyText>
+          </View>
         ),
         tabBarStyle: {
           backgroundColor: "#dfe4e0", 
@@ -36,10 +29,10 @@ export default function TabRoot() {
           color: '#dfe4e0'
         }
       }} 
-      >
-        <Tabs.Screen name = "index" options={{title: "Home", tabBarIcon: ({ color }) => <AntDesign size={28} name="home" color={color}/> }}/>
-        <Tabs.Screen name = "bookmarks" options={{title: "Bookmarks", tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color}/>}}/>
-        <Tabs.Screen name = "chats" options={{title: "Chats", tabBarIcon: ({ color }) => <AntDesign size={28} name="message1" color={color} />}}/>
+    >
+      <Tabs.Screen name="index" options={{title: "Home", tabBarIcon: ({ color }) => <AntDesign size={28} name="home" color={color}/> }} />
+      <Tabs.Screen name="bookmarks" options={{title: "Bookmarks", tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color}/>}}/>
+      <Tabs.Screen name="chats" options={{title: "Chats", tabBarIcon: ({ color }) => <AntDesign size={28} name="message1" color={color} />}}/>
     </Tabs>
   )
 }
@@ -48,12 +41,13 @@ const styles = StyleSheet.create({
   logo: {
     width: 35,
     height: 35,
-    borderRadius: 25  
-  },
-  userImage: {
-    width: 30,
-    height: 30,
     borderRadius: 25,
-    marginRight: 16,
+    marginRight: 8
+  },
+  headerCenter: {
+    flexDirection: 'row',
+    paddingLeft: "43%",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 })
